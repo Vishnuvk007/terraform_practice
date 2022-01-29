ba08936f482ad65c9e179e78e2bd2cb3b7a08283
Vishnu <vishnu19927@gmail.com>
2022-01-29 20:02:21 +0530

    upload splat

---
+++ section2/splat.tf
@@ -0,0 +1,16 @@
+/*
+variable "names" {
+  type = list
+  default = ["lb1","lb2","lb3"]
+}
+
+resource "aws_iam_user" "lb" {
+  name = var.names[count.index]
+  path = "/system/"
+  count = 3
+}
+
+output "iam" {
+    value = aws_iam_user.lb[*].arn
+}
+*/
